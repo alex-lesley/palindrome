acb6789c5795335180b2b1c4e26c7a90a3171b8c
alex lesley <alex_lesley@mail.ru>
2019-03-23 04:10:19 +0200

    palindrome check in js

--- palmax.js
+++ palmax.js
@@ -1,13 +1,18 @@
+function mark(char) {
+    if(char<"A" || (char>"Z" && char<"a") || (char>"z" && char<"À")) {
+        return true
+    } else {
+        return false
+    }
+}
 function is_palindrom(pal) {
     last=pal.length-1;
     if(last<1) {
         return true
     } else {
-        firscode=pal.charCodeAt(0);
-        lastcode=pal.charCodeAt(last);
-        if(firscode<65 || (firscode>90 && firscode<97) || (firscode>123 && firscode<191)) {
+        if(mark(pal[0])) {
             return is_palindrom(pal.substring(1))
-        } else if(lastcode<65 || (lastcode>90 && lastcode<97) || (lastcode>123 && lastcode<191)) {
+        } else if(mark(pal[last])) {
             return is_palindrom(pal.substring(0, last))
         } else {
             if(pal[0].toLowerCase() == pal[last].toLowerCase()) {
